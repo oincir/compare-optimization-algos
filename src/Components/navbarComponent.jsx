7fa677fc619756a499bcf80d4e715e5a6f947fb9
osmanincir <osmanincir.bm@gmail.com>
2021-05-12 23:04:43 +0300

    upload component added

--- src/Components/navbarComponent.jsx
+++ src/Components/navbarComponent.jsx
@@ -1,16 +1,17 @@
 import React, {Component} from 'react';
 import { Nav,Navbar } from "react-bootstrap";
+import { GiArtificialHive } from "react-icons/gi"
 
 class NavbarComponent extends Component {
     render() {
         return (
             <Navbar bg="light" expand="sm">
-                <Navbar.Brand href="home">[Logo]</Navbar.Brand>
+                <Navbar.Brand href="home"><h1><GiArtificialHive/></h1></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse>
                     <Nav className="mr-auto">
                         <Nav.Link href="home">Anasayfa</Nav.Link>
-                        <Nav.Link href="optimization">Optimizasyon Algoritmaları</Nav.Link>
+                        <Nav.Link href="optimization">Optimizasyon</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Navbar>
--- src/Components/uploadComponent.jsx
+++ src/Components/uploadComponent.jsx
@@ -1,20 +1,24 @@
 import React, {Component} from 'react';
-import { Card, Form } from "react-bootstrap";
+import {Card, Form, ProgressBar} from "react-bootstrap";
 
 class UploadComponent extends Component {
     render() {
         return (
-            <Card className={"flex justify-content-center align-items-center "}>
-                <Card.Body>
-                    <Form.Group className={"was-validated "}>
-                        <div className={"custom-file "}>
-                            <input type={"file"} className={"custom-file-input"} id={"validCustomFile"} required/>
-                            <label className={"custom-file-label"}>Dosya Seç...</label>
-                            <div className={"invalid-feedback"}> Lütfen Dosyayı ekleyiniz.</div>
-                        </div>
-                    </Form.Group>
-                </Card.Body>
-            </Card>
+            <div className={"flex"}>
+                <Card className={"justify-content-center align-items-center flex border-0"}>
+                    <Card.Body className={"col-md-8 "}>
+                        <Form.Group className="input-group mb-3 was-validated">
+                            <input type="file" className="form-control"
+                                   aria-label="Text input with segmented dropdown button" id={"validCustomFile"} required/>
+                            <div className="input-group-append">
+                                <button type="button" className="btn btn-outline-secondary" >Karşılaştır</button>
+                            </div>
+                            <div className={"invalid-feedback"}>Lütfen Dosyayı ekleyiniz.</div>
+                        </Form.Group>
+                        <ProgressBar variant="success" animated now={45} />
+                    </Card.Body>
+                </Card>
+            </div>
         );
     }
 }
